278e848374e93a8e7b8e946a28b70ce6369bb3f7
Sudeep Sharma <sharma.sudip1122@gmail.com>
2023-09-21 16:36:18 +0545

    First Iteration

---
+++ src/components/ListDisplay/ListDisplayPeople.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import "./ListDisplayPeople.css";
+
+function ListDisplayPeople() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/people.json")
+      .then((response) => response.json())
+      .then((jsonData) => setData(jsonData))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div id="display-table">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Department</th>
+            <th>Title</th>
+            <th>Computer</th>
+            <th>Office</th>
+            <th>Hire Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((employee, index) => (
+            <tr key={index}>
+              <td>{employee.Name}</td>
+              <td>{employee.Email}</td>
+              <td>{employee.Department}</td>
+              <td>{employee.Title}</td>
+              <td>{employee.Computer}</td>
+              <td>{employee.Office}</td>
+              <td>{employee.HireDate}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default ListDisplayPeople;
